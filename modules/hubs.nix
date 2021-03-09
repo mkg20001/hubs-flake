e32695969d42de020563a51ce99273dba9c8f74a
Maciej Krüger <mkg20001@gmail.com>
2021-03-09 08:54:41 +0100

    feat: add hacky patches that kinda work

--- modules/hubs.nix
+++ modules/hubs.nix
@@ -57,6 +57,88 @@ in
     };
 
     services.hubs.config = {
+      ret = {
+        "Elixir.Ret" = {
+          pool = "default";
+        };
+
+        "Elixir.RetWeb.Plugs.HeaderAuthorization" = {
+          header_value = "@@secret_phx_admin_key@@";
+        };
+
+
+        "Elixir.RetWeb.Endpoint".https = {
+          port = "4544";
+          # certfile
+          # cacertfile
+          # keyfile
+        };
+
+        "Elixir.RetWeb.Endpoint" = {
+          allowed_origins = "*";
+          secret_key_base = "@@phx_secret_base@@";
+          allow_crawlers = true;
+        };
+
+
+        "Elixir.Ret.Repo" = {
+          username = "root";
+          database = "ret_production";
+          socket_dir = "/run/postgresql";
+          template = "template0";
+          pool_size = 16;
+        };
+
+        "Elixir.Ret.SessionLockRepo" = {
+          username = "root";
+          database = "ret_production";
+          socket_dir = "/run/postgresql";
+          template = "template0";
+          pool_size = 16;
+        };
+
+
+        "Elixir.Ret.Locking".session_lock_db = {
+          username = "root";
+          database = "ret_production";
+          socket_dir = "/run/postgresql";
+          template = "template0";
+          pool_size = 16;
+        };
+
+
+        "Elixir.Ret.Guardian" = {
+          secret_key = "@@guardian_secret@@";
+        };
+
+        "Elixir.Ret.PermsToken" = {
+          perms_key = "@@permstoken@@";
+        };
+
+        bot_access_key = "@@bot_access_key@@";
+
+
+        "Elixir.Ret.PageOriginWarmer" = {
+          # pkgs.hubs.client
+          hubs_page_origin = "https://${cfg.domain}/_assets/client";
+          # pkgs.spoke.out
+          spoke_page_origin = "https://${cfg.domain}/_assets/spoke";
+          # pkgs.hubs.admin
+          admin_page_origin = "https://${cfg.domain}/_assets/admin";
+        };
+
+        "Elixir.Ret.HttpUtils" = {
+          insecure_ssl = !cfg.enableAcme;
+        };
+
+
+      run = {
+        hostname_dns_suffix = cfg.domain;
+      };
+
+      hackney = {
+        max_connections = 50;
+      };
     };
 
     services.postgresql = {
@@ -95,6 +177,46 @@ in
         # MY_VAR = "my_var";
       };
 
+script = ''
+export HOME="$RELEASE_TMP"
+export LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export REPLACE_OS_VARS=true # Inlines OS vars into vm args
+export MIX_ENV=prod
+
+SECRET_DIR="$RELEASE_TMP/secret"
+RELEASE_CONFIG_DIR="$RELEASE_TMP/config"
+
+CONFIG=$(echo ${escapeShellArg configJSON})
+
+mkdir -p "$SECRET_DIR"
+
+ulimit -a
+
+for sec in $(echo "$CONFIG" | grep -o "@@[a-zA-Z_][a-zA-Z_]*@@"); do
+  sec=''${sec//"@@"/""}
+  if [ ! -e "$SECRET_DIR/$sec" ]; then
+    dd if=/dev/urandom of=/dev/stdout count=64 bs=1 2>/dev/null | base64 -w 0 > "$SECRET_DIR/$sec"
+  fi
+  secv=$(cat "$SECRET_DIR/$sec")
+  CONFIG=''${CONFIG//"@@$sec@@"/"$secv"}
+done
+
+if [ ! -e "$RELEASE_TMP/.erlang.cookie" ]; then
+  dd if=/dev/urandom of=/dev/stdout count=64 bs=1 2>/dev/null | base64 -w 0 > "$RELEASE_TMP/.erlang.cookie"
+  chmod 0400 "$RELEASE_TMP/.erlang.cookie"
+fi
+
+rm -rf "$RELEASE_CONFIG_DIR"
+mkdir "$RELEASE_CONFIG_DIR"
+echo "$CONFIG" | ${pkgs.yj}/bin/yj -jt > "$RELEASE_CONFIG_DIR/config.toml"
+
+export PATH=${lib.makeBinPath (with pkgs; [ gawk bash coreutils gnused gnugrep ])}
+
+export NODE_NAME=$(echo "$HOSTNAME") # $(echo $HOSTNAME | sed "s|\\..+||g")
+export NODE_COOKIE="$NODE_NAME.cookie"
+
+RELEASE_MUTABLE_DIR="$RELEASE_TMP/mutable" RELEASE_CONFIG_DIR="$RELEASE_TMP/config" ${reticulum}/bin/ret start
+'';
       serviceConfig = {
         Type = "exec";
         User = "hubs";
--- pkgs/reticulum.nix
+++ pkgs/reticulum.nix
@@ -24,6 +24,29 @@ beam.packages.erlang.buildMix ({
     git
   ];
 
+  postPatch = ''
+    sed "s|hostname: \"localhost\"|socket_dir: \"/run/postgresql\"|g" -i config/prod.exs
+    sed "s|username: \"postgres\",|username: \"root\",|g" -i config/prod.exs
+    sed "s|password: \"postgres\",|show_sensitive_data_on_connection_error: true,|g" -i config/prod.exs
+
+    sed "s%password = session_lock_db_config |> Keyword.get(:password)%socket_dir = session_lock_db_config |> Keyword.get(:socket_dir)%" -i ./lib/ret/locking.ex
+    sed "s|password: password,|socket_dir: socket_dir,|" -i ./lib/ret/locking.ex
+
+    echo 'config :ret, Ret.Habitat, ip: "127.0.0.1", http_port: 9631' >> config/prod.exs
+
+    sed 's|config :peerage, via: Ret.PeerageProvider||g' -i config/prod.exs
+
+    cat config/prod.exs
+  '';
+
+  installPhase = ''
+    runHook preInstall
+    mix do compile --no-deps-check, distillery.release
+    mkdir -p $out
+    cp -a _build/prod/rel/ret/* $out
+    runHook postInstall
+  '';
+
   /* postBuild = ''
     for bin in $out/bin/*; do
       wrapProgram $bin --prefix PATH : ${lib.makeBinPath extraPath}
