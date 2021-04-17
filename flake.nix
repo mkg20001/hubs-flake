f7a3b3bc7c6f34425c658ae8a978bf8bb04463e7
Maciej Krüger <mkg20001@gmail.com>
2021-04-17 17:13:37 +0200

    feat: spellycaptor

--- flake.nix
+++ flake.nix
@@ -1,25 +1,27 @@
 {
   description = "Mozilla Hubs Cloud";
 
-  # inputs.nixpkgs.url = "github:happysalada/nixpkgs/fix_build_mix";
-  # inputs.nixpkgs.url = "github:mkg20001/nixpkgs/r_fix_build_mix";
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   inputs.nix-node-fod.url = "github:mkg20001/nix-node-fod/master";
+  inputs.speelycaptor.url = "github:mkg20001/speelycaptor-hapi/master";
+  inputs.speelycaptor.inputs.nixpkgs.follows = "nixpkgs";
 
-  outputs = { self, nixpkgs, nix-node-fod }:
+  outputs = { self, nixpkgs, nix-node-fod, speelycaptor }:
 
     let
       supportedSystems = [ "x86_64-linux" ];
       forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
 
       pkgs = forAllSystems (system: (import nixpkgs {
-       inherit system;
-       overlays = [ self.overlay ];
+        inherit system;
+        overlays = [ self.overlay ];
       }));
     in
 
     {
       overlay = import ./overlay.nix nix-node-fod;
+
+      overlays = [ self.overlay speelycaptor.overlay ];
 
       defaultPackage = forAllSystems (system: pkgs.${system}.reticulum);
 
@@ -30,6 +32,7 @@
       nixosModules.hubs = import ./modules/hubs.nix;
       nixosModules.janus = import ./modules/janus.nix;
       nixosModules.yt-dl-api-server = import ./modules/yt-dl-api-server.nix;
+      nixosModules.speelycaptor = speelycaptor.nixosModules.speelycaptor;
 
     };
 }
